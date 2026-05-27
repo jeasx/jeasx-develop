8d91c7d0a58f3ec6e4dea70a10e59e405a2ec71d
Maik Jablonski <jablonski@neoskop.de>
2026-05-27 15:51:21 +0200

    Serve static assets from root guard

--- src/[...guard].js
+++ src/[...guard].js
@@ -1,3 +1,13 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+
+if (!globalThis.assetPathCache) {
+  globalThis.assetPathCache = new Set();
+}
+
+/** @type Set<string> */
+const assetPathCache = globalThis.assetPathCache;
+
 /**
  * @param {import("./types").RouteProps} props
  */
@@ -5,4 +15,24 @@ export default function ({ request, reply }) {
   // Prepare "this" context
   this.request = request;
   this.reply = reply;
+
+  this.responseHandler = async (payload) => {
+    if (reply.statusCode === 404) {
+      if (!assetPathCache.has(request.path)) {
+        for (const prefix of ["dist", "public"]) {
+          try {
+            await fs.access(path.join(process.cwd(), prefix, request.path), fs.constants.R_OK);
+            assetPathCache.add(request.path);
+            break;
+          } catch {}
+        }
+      }
+      if (assetPathCache.has(request.path)) {
+        reply.code(200);
+        return reply.sendFile(request.path);
+      }
+    }
+
+    return payload;
+  };
 }
