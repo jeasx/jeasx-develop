756010a494149221df4778db56c1a587669250bd
Maik Jablonski <jablonski@neoskop.de>
2026-05-28 18:58:22 +0200

    use fs.glob for build asset path lookup

--- src/[...guard].js
+++ src/[...guard].js
@@ -1,8 +1,20 @@
 import fs from "node:fs/promises";
-import path from "node:path";
 
-const CWD = process.cwd();
-const STATIC_PATH_CACHE = new Map();
+const ASSET_PATHS = new Set(
+  (
+    await Promise.all([
+      Array.fromAsync(fs.glob("**/*.*", { cwd: "public" })),
+      Array.fromAsync(
+        fs.glob("**/*.*", {
+          cwd: "dist",
+          exclude: (name) => /\[.*\]/.test(name),
+        }),
+      ),
+    ])
+  )
+    .flat()
+    .map((filepath) => `/${filepath}`),
+);
 
 /**
  * @param {import("./types").RouteProps} props
@@ -14,26 +26,7 @@ export default async function ({ request, reply }) {
 
   // Example for serving static assets from root guard.
   // Requires {"serve": false} for FastifyStaticOptions.
-
-  if (!request.path.endsWith("/")) {
-    // Check if file exists
-    if (!STATIC_PATH_CACHE.has(request.path)) {
-      let isFile = false;
-      for (const folder of ["dist", "public"]) {
-        try {
-          const filepath = path.join(CWD, folder, request.path);
-          if ((await fs.stat(filepath)).isFile()) {
-            isFile = true;
-            break;
-          }
-        } catch {}
-      }
-      STATIC_PATH_CACHE.set(request.path, isFile);
-    }
-
-    // If path exists, serve asset
-    if (STATIC_PATH_CACHE.get(request.path)) {
-      return reply.sendFile(request.path);
-    }
+  if (ASSET_PATHS.has(request.path)) {
+    return reply.sendFile(request.path);
   }
 }
