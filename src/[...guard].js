db0a440ab7b8c6a0b5fe0f39bf86a237c6c2bc1e
Maik Jablonski <jablonski@neoskop.de>
2026-05-28 10:46:01 +0200

    Optimized root guard for static file serving

--- src/[...guard].js
+++ src/[...guard].js
@@ -4,37 +4,39 @@ import path from "node:path";
 /**
  * @param {import("./types").RouteProps} props
  */
-export default function ({ request, reply }) {
+export default async function ({ request, reply }) {
   // Prepare "this" context
   this.request = request;
   this.reply = reply;
 
-  this.responseHandler = async (payload) => {
-    if (reply.statusCode === 404) {
-      if (!globalThis.$filePathCache) {
-        globalThis.$filePathCache = new Set();
-      }
+  /*
+    Serve static assets from root guard.
+    Requires {"serve": false} for FastifyStaticOptions.
+  */
 
-      /** @type Set<string> */
-      const cache = globalThis.$filePathCache;
+  // Global cache for resolved asset paths
+  if (!globalThis.$filePathCache) {
+    globalThis.$filePathCache = new Set();
+  }
 
-      if (!cache.has(request.path)) {
-        for (const folder of ["dist", "public"]) {
-          try {
-            const filepath = path.join(process.cwd(), folder, request.path);
-            await fs.access(filepath, fs.constants.R_OK);
-            cache.add(request.path);
-            break;
-          } catch {}
-        }
-      }
+  /** @type Set<string> */
+  const cache = globalThis.$filePathCache;
 
-      if (cache.has(request.path)) {
-        reply.code(200);
-        return reply.sendFile(request.path);
-      }
+  // Check if file exists
+  if (!cache.has(request.path)) {
+    for (const folder of ["dist", "public"]) {
+      try {
+        const filepath = path.join(process.cwd(), folder, request.path);
+        await fs.access(filepath, fs.constants.R_OK);
+        cache.add(request.path);
+        break;
+      } catch {}
     }
+  }
 
-    return payload;
-  };
+  // If path exists, serve asset
+  if (cache.has(request.path)) {
+    reply.code(200);
+    return reply.sendFile(request.path);
+  }
 }
