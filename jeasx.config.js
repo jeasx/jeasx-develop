53312fe37f12ae5f4ff3b269f83e3b05f8189f4f
Maik Jablonski <mail@mjablonski.de>
2026-07-28 14:00:50 +0200

    explicit .BUILD_TIME file

--- jeasx.config.js
+++ jeasx.config.js
@@ -7,7 +7,9 @@ import rehypePrismPlus from "rehype-prism-plus";
 const NODE_ENV_IS_DEVELOPMENT = process.env.NODE_ENV === "development";
 
 if (process.env.BUILD_TIME) {
-  fs.writeFileSync(".env.local", `BUILD_TIME=${process.env.BUILD_TIME}`);
+  fs.writeFileSync(".BUILD_TIME", process.env.BUILD_TIME);
+} else {
+  process.env.BUILD_TIME = fs.readFileSync(".BUILD_TIME", "utf8");
 }
 
 export default {
